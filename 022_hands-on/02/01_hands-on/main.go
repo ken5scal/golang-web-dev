bab77e2a25a52e12c9830aa08d7184fc1faedbde
ken5scal <kengoscal@gmail.com>
2017-01-29 22:20:34 +0900

    Update

--- 022_hands-on/02/01_hands-on/main.go
+++ 022_hands-on/02/01_hands-on/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"net"
+	"log"
+	"io"
+)
+
 func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer listener.Close()
+
+	for {
+		c, err := listener.Accept()
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+
+		io.WriteString(c, "I see you connected.\n")
+		c.Close()
+	}
 }
