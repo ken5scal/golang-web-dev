56925b28461fa8fe5dff5a72fefc834feedf732f
ken5scal <kengoscal@gmail.com>
2017-01-30 23:09:20 +0900

    Solved

--- 024_hands-on/01_hands-on/main.go
+++ 024_hands-on/01_hands-on/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"html/template"
+	"log"
 )
 
 func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/dog.jpg", dog2)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -16,5 +19,14 @@ func foo(w http.ResponseWriter, r *http.Request) {
 }
 
 func dog(w http.ResponseWriter, r *http.Request) {
+	tpl , err := template.ParseFiles("dog.gohtml")
+	if err != nil {
+		log.Fatalln("error parsing template", err)
+	}
+	tpl.ExecuteTemplate(w, "dog.gohtml",nil)
+
+}
+
+func dog2(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "dog.jpg")
 }
