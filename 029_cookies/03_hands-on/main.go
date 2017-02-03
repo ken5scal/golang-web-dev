115799f2eb44776b8d944ccfe4da5518f9e12966
ken5scal <kengoscal@gmail.com>
2017-02-03 23:59:00 +0900

    Good

--- 029_cookies/03_hands-on/main.go
+++ 029_cookies/03_hands-on/main.go
@@ -1,4 +1,14 @@
 package main
 
+import "net/http"
+
+var counter int
+
 func main() {
+	http.HandleFunc("/", incrementCookie)
+	http.ListenAndServe(":8080", nil)
+}
+
+func incrementCookie(res http.Response, req *http.Request) {
+
 }
